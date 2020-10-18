cd9d01e0683c2f64970fcebf2d0dcb8c5eb94776
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2020-10-18 19:24:56 -0300

    MVP quase finalizado

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 $(function () {
-    $.getJSON("data/menu.json" , function(json){
+    $.getJSON('data/menu-mana.json' , function(json){
         $('#menu').html(Mustache.render($('#menu-template-1').html(), json));
+    });
+
+    $('#order').on('click', function() {
+    alert('Clicou no botao de fazer o pedido');
     });
 });
