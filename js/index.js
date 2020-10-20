070ef3ace02c68e6adc263f568058bf7f9b34ba2
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2020-10-20 17:17:10 -0300

    Carregando o json com base no parâmetro "e"

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 $(function () {
-    $.getJSON('data/menu-mana.json' , function(json){
-        $('#menu').html(Mustache.render($('#menu-template-1').html(), json));
-    });
+    const e = Global.getEventAlias();
+
+    $.getJSON('data/menu-' + e + '.json')
+        .done(data => {
+            $('#menu').html(Mustache.render($('#menu-template-1').html(), data));
+        })
+        .fail(() => location.href = Global.buildUrl('fail'));
 
     $('#order').on('click', function() {
         location.href = Global.buildUrl('order');
